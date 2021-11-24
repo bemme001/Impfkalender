635f01d2a8b3ba7ac7a106c85762461b9d95cd42
gbukov <gleb.liffe@gmail.com>
2021-11-24 16:25:43 +0100

    fixed: navbar position; added: link for the general view

--- src/Header/NavbarMain.js
+++ src/Header/NavbarMain.js
@@ -4,12 +4,13 @@ import {Link} from "react-router-dom";
 
 const NavbarMain = () => {
   return (
-    <Navbar bg="primary" variant="dark">
+    <Navbar bg="primary" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Brand href="/">LOGO</Navbar.Brand>
         <Nav className="d-flex">
           <Link to="/" className="nav-link">Startseite</Link>
           <Link to="/stiko-empfehlungen" className="nav-link">STIKO Impfempfehlung</Link>
+          <Link to="/generelle-eubersicht" className="nav-link">Generelle Übersicht</Link>
         </Nav>
       </Container>
     </Navbar>
