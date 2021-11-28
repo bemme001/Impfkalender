34bb0c472c95360fef32bd93d45a47a1a36986d5
Tobias Schulz <tobias-schulz-96@hotmail.de>
2021-11-28 14:16:42 +0100

    removed unused code

--- src/GeneralOverview/MainView.js
+++ src/GeneralOverview/MainView.js
@@ -11,60 +11,6 @@ import useEffectAsync from "../hooks/useEffectAsync";
 
 const id = 2691490;
 
-/* var numberOfImz = 3;//Länge des Arrays nachpflegen
-var iterationPoint = 0;
-
-function generateStructureOfTiles(props){
-    //generate Tiles and fill up with empty Cols if needed.
-    let listOfTileGroups = [];
-    let counter = 0;
-    let maxLength = 0;
-    //ermittle maximale Menge an Cols anhand von numberOfImz % 5 === 0 (wenn ungerade, fülle auf bis gerade)
-    if(numberOfImz % 5 === 0){
-        maxLength = numberOfImz;
-    } else {
-        let add = 0;
-        while((numberOfImz+add) % 5 !== 0){
-            add++;
-        }
-        maxLength = numberOfImz+add; //Länge der Imz + filler
-    }
-} */
-
-/* function Generate(props){
-    let tiles = []
-    let counter = 0;
-    //Wenn die differenz zwischen iterationPoint+5 und numberOfImz größer 1 und kleiner 5 ist, fülle den Rest mit leeren <Col> auf.
-    
-    for (let i = 1; i <= 5; i++) {
-      if(counter < numberOfImz-iterationPoint){
-        tiles.push(<Col ><VaccinationTiles title  ="Malaria" VacType="G1"/></Col>);
-        counter++;
-      }else{
-        tiles.push(<Col ></Col>);
-      }
-    }
-    iterationPoint += 5;
-    console.log(tiles);
-    return tiles;
-}
-
-function GenerateImmunizationTilesStructure(props) {
-
-    let rowList = [];
-
-    for (let i = 1; i <= numberOfImz; i++) {
-        if(i === 1 || i === 6 || i === 11 || i === 16 || i === 21){
-            rowList.push(
-                <Row className="equal px-4" key={i}>
-                  {Generate()}
-                </Row>
-            );      
-        }  
-    }
-return 1;
-   
-} */
 
 export default function MainView() {
 
@@ -74,7 +20,7 @@ export default function MainView() {
     //const [year, setYear] = useState(60);
     //const [month, setMonth] = useState(24)
     //let patientAge;
-    //let numOfImz;
+
 
     useEffectAsync( async () => {
         const p = await Patient.create(id);
@@ -83,7 +29,6 @@ export default function MainView() {
 
         const i = await Immunization.create(cacheuuid.current);
         setImmunization(i);
-        //if(immunization) numOfImz = Object.keys(immunization).length;//Undefined Problem
     },[]);
 
     const immunizationTiles = () => {
@@ -107,31 +52,6 @@ export default function MainView() {
         }
         return null;
     }
-
-    /*function check(counter) {
-        if(immunization){
-            return immunization[counter];
-        }
-        return undefined;
-    }
-
-
-  
-    function returnOneTile(counter){
-        return "<Col ><VaccinationTiles immunization={check(" + {counter} + ")}/></Col>"
-    }
-    
-    function returnRow(){     
-        
-        //console.log(Immunization.imzLength);
-        let e = ["<Col ><VaccinationTiles immunization={check(i)}/></Col>", "<Col ><VaccinationTiles immunization={check(i)}/></Col>"]
-        for (let i = 0; i < 5; i++) {
-            //TODO
-        }
-      
-        return <Row className="equal px-4"><Col></Col></Row>
-    }*/
-
 
     return (
         <div>
