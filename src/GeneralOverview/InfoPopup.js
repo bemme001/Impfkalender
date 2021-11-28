399f2f2d886d7dc5bb20fc49ff29cc27554a89b6
valentin-schoeneich <valentin.schoeneich@student.hs-rm.de>
2021-11-28 13:32:42 +0100

    Added popup window for tiles

---
+++ src/GeneralOverview/InfoPopup.js
@@ -0,0 +1,20 @@
+import {Button, Modal} from "react-bootstrap";
+
+const InfoPopup = (props) => {
+
+  return (
+    <div className='InfoPopup'>
+      <Modal show={props.showPopUp} onHide={props.switchPopUp} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>Infos zur Impfung</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>testestestestest</p>
+          <Button onClick={props.switchPopUp}>Schließen</Button>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+}
+
+export default InfoPopup;
--- src/GeneralOverview/VaccinationTiles.js
+++ src/GeneralOverview/VaccinationTiles.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState} from 'react';
 import {Card,Button} from "react-bootstrap";
+import InfoPopup from "./InfoPopup";
 
 const VaccinationTiles = ({ title, VacType, display="d-inline" }) => {
+  const [showPopUp, setShowPopUp] = useState(false);
+
+  const switchPopUp = () => {
+    setShowPopUp(show => !show);
+  }
+
   return (
      <div className="mb-4">
         <Card border="info" class={display}>
         <Card.Header><h4>{title}</h4></Card.Header>
         <Card.Body>
             <Card.Title>{VacType}</Card.Title>
-            <Button variant="outline-info" className="w-100">Öffnen</Button>
+            <Button variant="outline-info" onClick={switchPopUp} className="w-100">Öffnen</Button>
+            {showPopUp ? <InfoPopup showPopUp={showPopUp} switchPopUp={switchPopUp}/> : null}
         </Card.Body>
     </Card>
     </div> 
