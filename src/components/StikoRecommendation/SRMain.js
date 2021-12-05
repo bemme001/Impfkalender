a460e0ec6ac8fb55ed64f2eab465ef5a3d750ed6
gbukov <gleb.liffe@gmail.com>
2021-12-05 17:07:59 +0100

    Added: color support for cells

--- src/components/StikoRecommendation/SRMain.js
+++ src/components/StikoRecommendation/SRMain.js
@@ -3,28 +3,43 @@ import {Container, Row} from "react-bootstrap";
 import SRTable from "./SRTable";
 import SrTableDescription from "./SRTableDescription";
 import SR2021 from "./SR2021";
-import Immunization from "../GeneralOverview/Immunization";
-import useEffectAsync from "../../hooks/useEffectAsync";
-import Patient from "../GeneralOverview/Patient";
 
 const SRMain = () => {
 
   const recommendation = SR2021()
 
   // Testbereich -- Start
-  const [immunization, setImmunization] = useState(null);
-  useEffectAsync( async () => {
-    const p = await Patient.create(2698452);
-    const i = await Immunization.create(p.uuid);
-    setImmunization(i);
-  },[]);
+  // const [immunization, setImmunization] = useState(null);
+  // useEffectAsync( async () => {
+  //   const p = await Patient.create(2698452);
+  //   console.log(p)
+  //   const i = await Immunization.create(p.uuid);
+  //   setImmunization(i);
+  // },[]);
+
+  // function diffInMonths(birthDate, immunDate) {
+  //   let date1 = new Date(birthDate)
+  //   let date2 = new Date(immunDate)
+  //   let years = date2.getFullYear() - date1.getFullYear()
+  //   let months = (years * 12) + (date2.getMonth() - date1.getMonth())
+  //   return months
+  // }
+  //
+  // console.log('1: ' + diffInMonths("1988-10-21", "1988-11-25"))
+  // console.log('2: ' + diffInMonths("1988-10-21", "1989-01-05"))
+  //
+  // const patient = {id: 1, birthdate: "1988-10-21"}
+  // const immunization = [
+  //   {id: 1, pathogen: "Rotaviren", status: "completed", immun: "G1", date: "1988-11-25"},
+  //   {id: 2, pathogen: "Rotaviren", status: "completed", immun: "G2", date: "1989-01-05"},
+  // ]
   // Testbereich -- End
 
   return (
     <Container fluid="xl">
       <Row className="bg-light mt-5 mb-5 rounded-3 pb-4 pt-5">
         <h6 className="mb-2" style={{color: "#4082df"}}>Tabelle 1: Impfkalender (Standardimpfungen) für Säuglinge, Kinder, Jugendliche und Erwachsene; 2020/2021</h6>
-        <SRTable recommendation={recommendation}/>
+        <SRTable recommendation={recommendation} />
         <SrTableDescription />
       </Row>
     </Container>
--- src/components/StikoRecommendation/SRTableRow.js
+++ src/components/StikoRecommendation/SRTableRow.js
@@ -1,15 +1,67 @@
 import React from 'react';
 
 const SrTableRow = ({vaccination}) => {
+  function diffInMonths(birthDate, immunDate) {
+    let date1 = new Date(birthDate)
+    let date2 = new Date(immunDate)
+    let years = date2.getFullYear() - date1.getFullYear()
+    let months = (years * 12) + (date2.getMonth() - date1.getMonth())
+    return months
+  }
+
+  // const patient = {id: 1, birthdate: "1988-10-21"}
+  const patient = {}
+  const immunizations = [
+    {id: 1, pathogen: "Rotaviren", status: "completed", immun: "G1", date: "1988-11-25"},
+    {id: 2, pathogen: "Rotaviren", status: "completed", immun: "G2", date: "1989-06-05"},
+    //{id: 2, pathogen: "Rotaviren", status: "completed", immun: "G3", date: "1989-06-05"},
+  ]
+
   return (
     <tr className="border border-white">
       <th scope="row" className="border border-gray">{vaccination.name}</th>
       {
         vaccination.times.map(e => {
-          if (e.name === 'close') return <td colSpan={e.end - e.start} className="bg-grey"/>
-          if (e.name === 'open')   return <td colSpan={e.end - e.start} className="bg-blue-light"/>
+          // check if the cell is a close/open/space
+          if (e.name === 'close')  return <td colSpan={e.t_end - e.t_start} className="bg-grey"/>
+          if (e.name === 'open' )  return <td colSpan={e.t_end - e.t_start} className="bg-blue-light"/>
           if (e.name === 'space')  return <td className="w-1"/>
-          return <td colSpan={e.end - e.start} className="bg-blue">{e.name} <span className="comment">{e.desc}</span></td>
+
+          const immunisation = immunizations.find(i => i.pathogen === vaccination.name && i.immun === e.name)
+          // first of all try to find vaccine
+          if (immunisation && Object.keys(patient).length !== 0) {
+            // calculate the date of vaccine
+            const vaccineDateDiffInMonths = diffInMonths(patient.birthdate, immunisation.date)
+            // calculate whether the vaccine was given on right time -> green, not -> orange
+            if (vaccineDateDiffInMonths >= e.r_start && vaccineDateDiffInMonths <= e.r_end) {
+              return <td
+                colSpan={e.t_end - e.t_start}
+                className="bg-green">{e.name}
+                <span className="comment"> {e.desc}</span>
+              </td>
+            } else {
+              return <td
+                colSpan={e.t_end - e.t_start}
+                className="bg-orange">{e.name}
+                <span className="comment"> {e.desc}</span>
+              </td>
+            }
+          }
+
+          // check if the patient should have been vaccinated
+          if (Object.keys(patient).length !== 0) {
+            const birthDateAndTodayDiffInMonths = diffInMonths(patient.birthdate, Date.now())
+            if (birthDateAndTodayDiffInMonths > e.r_end) {
+              return <td
+                colSpan={e.t_end - e.t_start}
+                className="bg-red">{e.name}
+                <span className="comment"> {e.desc}</span>
+              </td>
+            }
+          }
+
+          // or just show recommendation
+          return <td colSpan={e.t_end - e.t_start} className="bg-blue">{e.name} <span className="comment">{e.desc}</span></td>
         })
       }
     </tr>
