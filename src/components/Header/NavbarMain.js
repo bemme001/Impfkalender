0c94f05360a987079d55728dc79dedfac12109c4
gbukov <gleb.liffe@gmail.com>
2021-12-09 14:02:45 +0100

    Added: Responsive behaviors (collapse for small devices iPad etc.) to Navbar

--- src/components/Header/NavbarMain.js
+++ src/components/Header/NavbarMain.js
@@ -5,17 +5,20 @@ import Searchbar from "./Searchbar"
 
 const NavbarMain = () => {
   return (
-    <Navbar bg="primary" variant="dark" sticky="top">
+    <Navbar bg="primary" variant="dark" sticky="top" collapseOnSelect expand="lg">
       <Container>
         <Navbar.Brand href="#home">LOGO</Navbar.Brand>
-        <Nav>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="m-auto me-auto">
             <Searchbar />
-        </Nav>
-        <Nav className="d-flex">
-          <Link to="/" className="nav-link">Startseite</Link>
-          <Link to="/stiko-empfehlungen" className="nav-link">STIKO Impfempfehlung</Link>
-          <Link to="/generelle-uebersicht" className="nav-link">Generelle Übersicht</Link>
-        </Nav>
+          </Nav>
+          <Nav >
+            <Link to="/" className="nav-link">Startseite</Link>
+            <Link to="/stiko-empfehlungen" className="nav-link">STIKO Impfempfehlung</Link>
+            <Link to="/generelle-uebersicht" className="nav-link">Generelle Übersicht</Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
