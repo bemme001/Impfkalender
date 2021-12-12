124c6ae9f8844c0f421231a7fc89eb38bb573285
bemme001 <emmibe@gmx.de>
2021-12-12 19:09:04 +0100

    schönere Datumsanzeige im MainView

--- src/components/GeneralOverview/PatientInformations.js
+++ src/components/GeneralOverview/PatientInformations.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
+// schöne Date-Ausgabe
+const date_toString = (date) => {
+  const temp = date.split('-');
+  return [temp[2], temp[1], temp[0]].join(".");
+}
+
 const PatientInformation = (props) => {
 
     return (
       <div>
           <h1>{props.patient.name}</h1>
-          <h4>Geschlecht: {props.patient.gender} Geb: {props.patient.birthdate}</h4>
+          <h4>Geb: {date_toString(props.patient.birthdate)}</h4>
+          <h4>Geschlecht: {props.patient.gender}</h4>
           <h4>{props.patient.line}</h4>
           <h4>{props.patient.postal}, {props.patient.city}</h4>
           <h4>Tel.: {props.patient.telecom}</h4>
