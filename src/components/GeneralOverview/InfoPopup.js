3aedf64c0aca7521c9abd9002b2f2c9f8ee9f04b
mostafa_lamkadmi@gmx.de <mostafa>
2022-01-04 18:24:48 +0100

    UpdateInfoPopup

--- src/components/GeneralOverview/InfoPopup.js
+++ src/components/GeneralOverview/InfoPopup.js
@@ -1,4 +1,6 @@
 import { Button, Form, Modal, Col, Row } from "react-bootstrap";
+import { BsFillHandIndexFill } from "react-icons/bs";
+
 /*import {GlobalContext} from "../context/GlobalState";
 import React, {useRef, useState, useContext} from 'react'*/
 
@@ -10,6 +12,10 @@ const InfoPopup = (props) => {
     console.log(patientObject);
     console.log(immunizationList);
   }*/
+ function UpdateInfo (item, value){
+ 
+  props.infos[item] = value;
+ }
 
   return (
     <div className='InfoPopup'>
@@ -23,7 +29,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Erreger:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2" 
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('pathogen',e.target.innerText)} >
                 {props.infos.pathogen}
               </Col>
             </Form.Group>
@@ -32,7 +40,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Impfstoff:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('vaccine',e.target.innerText)} >
                 {props.infos.vaccine}
               </Col>
             </Form.Group>
@@ -41,7 +51,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Status:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('status',e.target.innerText)}>
                 {props.infos.status}
               </Col>
             </Form.Group>
@@ -50,7 +62,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Immunisierungsgrad:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('immun',e.target.innerText)}>
                 {props.infos.immun}
               </Col>
             </Form.Group>
@@ -59,7 +73,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Impfdatum:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('date',e.target.innerText)}>
                 {props.infos.date}
               </Col>
             </Form.Group>
@@ -68,7 +84,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Impfstelle:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('site',e.target.innerText)}>
                 {props.infos.site}
               </Col>
             </Form.Group>
@@ -77,7 +95,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Dosis in ml:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('quantity',e.target.innerText)}>
                 {props.infos.quantity}
               </Col>
             </Form.Group>
@@ -86,7 +106,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Impfgrund:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('reason',e.target.innerText)}>
                 {props.infos.reason}
               </Col>
             </Form.Group>
@@ -95,7 +117,9 @@ const InfoPopup = (props) => {
               <Form.Label column sm={4} md={4} lg={4}>
                 Bemerkung:
               </Form.Label>
-              <Col sm={4} md={4} lg={4} className="p-2">
+              <Col sm={4} md={4} lg={4} className="p-2"
+              contentEditable suppressContentEditableWarning 
+              onBlur={e =>UpdateInfo('note',e.target.innerText)}>
                 {props.infos.note}
               </Col>
             </Form.Group>
@@ -113,6 +137,7 @@ const InfoPopup = (props) => {
           {/*<p>{test()}</p>*/}
 
           <Button onClick={props.switchPopUp}>Schließen</Button>
+        
         </Modal.Body>
       </Modal>
     </div>
