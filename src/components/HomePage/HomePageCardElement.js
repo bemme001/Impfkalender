7dcffd5b7de23c1309c760aeba84133ec402f255
valentin-schoeneich <valentin.schoeneich@student.hs-rm.de>
2022-01-10 12:03:44 +0100

    searchbar changes

--- src/components/HomePage/HomePageCardElement.js
+++ src/components/HomePage/HomePageCardElement.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Button, Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import stikoImg from "./stiko-img.png";
+//import stikoImg from "./stiko-img.png";
 
 const HomePageCardElement = ({ extraInfo = "---",
                          name = "---",
@@ -12,7 +12,7 @@ const HomePageCardElement = ({ extraInfo = "---",
           backgroundImage: 'linear-gradient(135deg, #0d6efd 50%, #629df5 80%)', borderRadius: '25px' }}>
       <Card.Body>
         <Card.Title style={{fontSize: '30px'}}>{name}</Card.Title>
-        <Card.Img src={stikoImg} alt='Bild kann nicht angezeigt werden!' style={{borderRadius: '50%', width: '200px'}}/>
+        <Card.Img alt='Bild kann nicht angezeigt werden!' style={{borderRadius: '50%', width: '200px'}}/>
         <Card.Text>{description}</Card.Text>
         <Link to={link}>
           <Button variant="outline-light text-white">Öffnen</Button>
