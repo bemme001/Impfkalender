ff0823027c659174e1e06d9443a1ad8bd8f863c4
shenz001 <simon.henz@student.hs-rm.de>
2022-01-10 12:02:36 +0100

    added motion

--- src/components/HomePage/HomePageCardElement.js
+++ src/components/HomePage/HomePageCardElement.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import {Button, Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import stikoImg from "./stiko-img.png";
+import { motion } from "framer-motion";
+
+const MotionCard = motion(Card);
 
 const HomePageCardElement = ({ extraInfo = "---",
                          name = "---",
                          description = "---",
                          link = "---"}) => {
   return (
-    <Card className="text-center text-white" style={{ width: '30rem', backgroundColor: '#0d6efd',
+    <MotionCard whileHover={{scale:1.1}} whileTap={{scale:0.9}} className="text-center text-white" style={{ width: '30rem', backgroundColor: '#0d6efd',
           backgroundImage: 'linear-gradient(135deg, #0d6efd 50%, #629df5 80%)', borderRadius: '25px' }}>
       <Card.Body>
         <Card.Title style={{fontSize: '30px'}}>{name}</Card.Title>
-        <Card.Img src={stikoImg} alt='Bild kann nicht angezeigt werden!' style={{borderRadius: '50%', width: '200px'}}/>
+        <Card.Img style={{borderRadius: '50%', width: '200px'}}/>
         <Card.Text>{description}</Card.Text>
         <Link to={link}>
           <Button variant="outline-light text-white">Öffnen</Button>
         </Link>
       </Card.Body>
-    </Card>
+    </MotionCard>
   );
 };
 
