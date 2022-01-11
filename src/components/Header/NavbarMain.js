6a024bad7987179c9114d7bafc0bed2c4ad11e13
shenz001 <simon.henz@student.hs-rm.de>
2022-01-11 21:34:22 +0100

    Navbar change

--- src/components/Header/NavbarMain.js
+++ src/components/Header/NavbarMain.js
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
-import { Container, Nav, Navbar, Button } from "react-bootstrap";
-import {Link, useLocation} from "react-router-dom";
+import React from 'react';
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
 import Searchbar from "./Searchbar";
+import { motion } from "framer-motion";
+import logo from "./logo.png";
 // import { BsSearch } from 'react-icons/bs';
 
 const NavbarMain = () => {
   {/*const [searchButton, setSearchButton] = useState(false);*/}
 
-  return (
+  return (useLocation().pathname == '/' ? null : 
     <Navbar bg="primary" variant="dark" sticky="top">
       <Container>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Brand href="/">
+            <motion.img
+                src={logo}
+                width="35px"
+                height="35px"
+                alt="Home"
+                whileHover={{rotate: [0, 360], scale: 1.4}}
+                initial={{x: "-50vw"}}
+                animate={{x: 0, transition: {
+                    duration: 0.5
+                }}}
+            />
+        </Navbar.Brand>
         <Nav className="d-flex align-items-center">
           {/*searchButton && <Searchbar />*/}
-          {useLocation().pathname == '/' ? null : <Searchbar formStyleClass="text-white transparent-input"
-              buttonStyleClass="transparent-button" variantStyle="outline-light" inputSize="sm"/> }
+          <Searchbar formStyleClass="text-white transparent-input"
+              buttonStyleClass="transparent-button" variantStyle="outline-light" inputSize="sm"/>
           {/*<Link to="#" className="nav-link pt-1" onClick={() => setSearchButton(!searchButton)}><BsSearch /></Link>*/}
-          <Link to="/" className="text-white nav-link">Startseite</Link>
           <Link to="/stiko-empfehlungen" className="text-white nav-link">STIKO Impfempfehlung</Link>
           <Link to="/generelle-uebersicht" className="text-white nav-link">Generelle Übersicht</Link>
         </Nav>
